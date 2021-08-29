f066c14348fad8285be7f4a9bfeae04481e0b4d1
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-28 18:51:25 -0700

    Created register view model

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -13,6 +13,25 @@ namespace eCommerceSite.Models
         public string Email { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public DateTime? DateOfBirth { get; set; } // optional
+        public DateTime? DateOfBirth { get; set; } // Optional
+    }
+
+    public class RegisterViewModel
+    { // Does not go in DB do not add DbSet for this!!
+        [Required]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email))] // Might need to put required...
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))] // Might need to put required...
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // Time ignored
+        public DateTime? DateOfBirth { get; set; } // Optional
     }
 }
