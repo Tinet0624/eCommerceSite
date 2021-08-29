b66c687714fcbcf671506f9d515d6bd737fc90df
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-28 21:01:36 -0700

    Created Login Page Added Validation messages for Login

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,11 +39,52 @@ namespace eCommerceSite.Controllers
                 _context.UserAccounts.Add(account);
                 await _context.SaveChangesAsync();
                 // Say Hello!
-                TempData["Message"] = $"Hello {reg.UserName}!";
+                TempData["Message"] = $"Thank you for registering an account with us {reg.UserName}!";
                 // Redirect to home
                 return RedirectToAction("Index", "Home");
             }
             return View(reg);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LogInViewModel logIn)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(logIn);
+            }
+            UserAccount account = await (from u in _context.UserAccounts
+                                   where (u.UserName == logIn.UserNameOrEmail
+                                       || u.Email == logIn.UserNameOrEmail)
+                                   && u.Password == logIn.Password
+                                   select u).SingleOrDefaultAsync();
+
+            // Method syntax
+            //UserAccount acc = await _context.UserAccounts
+            //    .Where(userAcc => 
+            //          (userAcc.UserName == logIn.UserNameOrEmail 
+            //        || userAcc.Email == logIn.UserNameOrEmail)
+            //        && userAcc.Password == logIn.Password).SingleOrDefaultAsync();
+
+            if (account == null)
+            {
+                // Credentials did not match
+                ModelState.AddModelError(string.Empty, "Credentials were not found.");
+                // Error Message
+                TempData["Message"] = $"Im sorry your Login Information was not correct.";
+                return View(logIn);
+            }
+            // Say Hello!
+            TempData["Message"] = $"Welcome {account.UserName}!";
+            // Log User into website
+
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
