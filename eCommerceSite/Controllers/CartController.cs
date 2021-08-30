0c8e8a8b5da3705264016ce3b7de11a6b631c1f2
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-30 16:12:46 -0700

    Created basic summary page. Does not work with empty cookie

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -59,7 +59,11 @@ namespace eCommerceSite.Controllers
         public IActionResult Summary()
         {
             // Display all products in the shopping cart cookie
-            return View();
+            string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
+
+            List<Product> cartProducts = JsonConvert.DeserializeObject<List<Product>>(cookieData);
+
+            return View(cartProducts);
         }
     }
 }
